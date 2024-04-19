26d0c4b3bd97cf2baa354e8b7f9da9ea3d6995ab
“thelastgoth128” <bsc-com-04-22@unima.ac.mw>
2024-04-19 15:50:35 -0700

    question 6

---
+++ file.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+//function to capitialize second letter in FileData
+void CapitalizeSecondLetter(string& str) {
+    bool newWord = true; // Flag to track word boundaries
+
+    for (size_t i = 0; i < str.length(); ++i) {
+        if (newWord && islower(str[i])) {
+            str[i] = toupper(str[i]); // Capitalize the second letter
+            newWord = false;
+        }
+
+        if (str[i] == ' ') {
+            newWord = true; // Next character starts a new word
+        }
+    }
+}
+
+//Function reverse a string
+void Reverse(string& str){
+    reverse (str.begin(), str.end());
+}
+int main (){
+    int vowelcount=0;
+    string vowels = "AEIOUaeiuo";
+
+ifstream text("code.txt");
+   string FileData;
+
+if (text.is_open()){
+    string line;
+    while (getline(text,line)){
+        FileData += line;
+    }
+    text.close();
+    cout << FileData << endl;
+    for(char ch : FileData){
+        if (vowels.find(ch) != string::npos){
+            vowelcount++;
+        }
+    }
+}
+cout << "nubmer of vowesls in FileData is : " << vowelcount << endl;
+CapitalizeSecondLetter(FileData);
+cout <<" the capitalized second letter is as follows: " << FileData << endl; 
+
+
+Reverse(FileData);
+cout << "the reversed text: " << FileData << endl;
+
+return 0;
+}
